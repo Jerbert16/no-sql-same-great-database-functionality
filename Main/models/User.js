535cd79d062c682a5812af3a3753a0e4badb8fc7
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-04-29 16:39:25 -0600

    created user model

---
+++ Main/models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+const validateEmail = function (email) {
+  const redux = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return redux.test(email);
+};
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, "Please enter a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [{ thoughts_id: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ friends_id: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
