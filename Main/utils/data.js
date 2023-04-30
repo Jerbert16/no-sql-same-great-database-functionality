e08f4ac4ea604157c3fa3c4a4b9016d44f8eeeea
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-04-30 17:19:28 -0600

    first stab at data and seed utils

---
+++ Main/utils/data.js
@@ -0,0 +1,36 @@
+const users = [
+    'Nocodejones77',
+    'needTopractice32',
+    'abitofabear34',
+    'NeedSun345',
+    'Readyforsummer2000',
+  ];
+  
+  const thoughtDescriptions = [
+    'I need ice cream',
+    'Why do I do these things?',
+    'I am ready for vacation, too',
+    'Gonna be a long night',
+    'Always look on the bright side of life, (whistling)',
+  ];
+  
+  // Get a random item given an array
+  const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  
+  // Gets a random user
+  const getRandomName = () =>
+    `${getRandomArrItem(users)}`;
+  
+  // Function to generate random thought that we can add to user object.
+  const getRandomThought = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        thoughts: getRandomArrItem(thoughtDescriptions),
+      });
+    }
+    return results;
+  };
+  
+  // Export the functions for use in seed.js
+  module.exports = { getRandomUser, getRandomThought  };
---
+++ Main/utils/seed.js
@@ -0,0 +1,50 @@
+const connection = require('../config/connection');
+const { Thought, User } = require('../models');
+const { getRandomUser, getRandomThought } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Drop existing thoughts
+  await Thought.deleteMany({});
+
+  // Drop existing users
+  await User.deleteMany({});
+
+  // Create empty array to hold users
+  const users = [];
+
+  // Loop 20 times -- add users to array
+  for (let i = 0; i < 20; i++) {
+    // Get some random assignment objects using a helper function that we imported from ./data
+    const thoughts = getRandomThought(20);
+
+    const username = getRandomUser();
+    const email = username.split(' ')[0];
+    const friends = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+
+    users.push({
+      username,
+      email,
+      thoughts,
+      friends,
+    });
+  }
+
+  // Add users to the collection and await the results
+  await User.collection.insertMany(users);
+
+  // Add thoughts to the collection and await the results
+  await Thought.collection.insertOne({
+    thoughtText: 'I hope this works',
+    username: 'lostcoder12',
+    reactions: [...reactions],
+  });
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
